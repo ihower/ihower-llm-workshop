ce93b9a6130993cde318eac151da0d628d218d2b
Wen-Tien Chang <ihower@gmail.com>
2025-05-07 00:18:12 +0800

    agents fastapi

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ app.add_middleware(
 # 掛載靜態文件目錄
 app.mount("/static", StaticFiles(directory="static"), name="static")
     
-
 ## Form + Streaming
 @app.get("/api/v1/completion_stream")
 async def get_completion_stream(query: str):
@@ -97,16 +96,94 @@ async def generate_completion_json_stream(query: str):
                   # JSON 還不完整，繼續等待更多數據
                   pass
           elif event.type == "chunk" and event.chunk.usage:
-              usage = event.chunk.usage      
-              streaming_results["input"] = messages     
-              streaming_results["output"] = json.loads(json_str)                
-              streaming_results["metrics"]["prompt_tokens"] = usage.prompt_tokens
-              streaming_results["metrics"]["completion_tokens"] = usage.completion_tokens
-              streaming_results["metrics"]["total_tokens"] = usage.total_tokens
-              yield(streaming_results)
+              usage = event.chunk.usage
+              print(f"usage: {usage}")
+              print(f"final output: {json_str}")
     
-    yield "data: [DONE]\n\n"    
-
-
+    done_event = { "message": "DONE" }
+    yield f"data: {json.dumps(done_event)}\n\n"    
+ 
 
 ## Chat Agent + Streaming + Structured Output
+from agents import Agent, Runner, set_default_openai_key, function_tool, trace, ItemHelpers
+from openai.types.responses import ResponseTextDeltaEvent, ResponseCompletedEvent
+
+from tavily import TavilyClient
+
+tavily_client = TavilyClient()
+
+@function_tool
+def web_search(query: str) -> str:
+    """Search the web for information"""
+    return tavily_client.search(query)
+
+@function_tool
+def web_search(query: str):
+  """Call this function when recent data is required to perform a web search"""
+  print(f"  ⚙️ 呼叫函式 web_search 參數: {query}")
+  response = tavily_client.search(query)
+  return str(response)
+
+
+@app.get("/api/v1/agent_stream")
+async def get_agent_stream(query: str, previous_response_id: str = None):
+    response = StreamingResponse(generate_agent_stream(query, previous_response_id), media_type="text/event-stream")
+    response.headers["X-Accel-Buffering"] = "no"
+    return response
+
+async def generate_agent_stream(query: str, previous_response_id: str = None):
+
+    agent = Agent(
+        name="Shop Agent",
+        instructions= """You are a helpful assistant that can answer questions and help with tasks. Always respond in Traditional Chinese.""",
+        tools=[web_search],
+        model="gpt-4.1-mini",
+        output_type=QueryResult
+    )
+    print(f"previous_response_id: {previous_response_id}")
+
+    result = Runner.run_streamed(agent, input=query, previous_response_id=previous_response_id)
+
+    json_str = ''
+    previous_content = ''
+
+    async for event in result.stream_events():
+        print(event)
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            print(event.data.delta)
+
+            json_str += event.data.delta
+            try:
+                result = jiter.from_json(json_str.encode('utf-8'), partial_mode="trailing-strings")
+                
+                # 針對 content 只輸出 increment 最後文字
+                if "content" in result:
+                    current_content = result["content"]
+                    if current_content != previous_content:
+                        result["content"] = current_content[len(previous_content):]
+                        previous_content = current_content
+                    elif current_content == previous_content:
+                        result["content"] = ''
+                    
+                yield f"data: {json.dumps(result)}\n\n"
+            except ValueError:
+                # JSON 還不完整，繼續等待更多數據
+                pass
+        elif event.type == "raw_response_event" and isinstance(event.data, ResponseCompletedEvent):
+            print(f"last_response_id: {event.data}")
+            last_response_id = event.data.response.id
+        elif event.type == "run_item_stream_event":
+            if event.item.type == "tool_call_item":
+                print("-- Tool was called")
+            elif event.item.type == "tool_call_output_item":
+                print(f"-- Tool output: {event.item.output}")
+            elif event.item.type == "message_output_item":
+                print(f"-- Message output:\n {ItemHelpers.text_message_output(event.item)}")                
+            else:
+                pass  # Ignore other event types            
+
+    done_event = { "message": "DONE", "last_response_id": last_response_id }
+    yield f"data: {json.dumps(done_event)}\n\n"    
+
+
+
